0836d546dc858dc6bc42e2561279fedf2da90659
“Stalin <stalinraja.sachin@gmail.com>
2024-08-07 05:17:35 +0000

    Small modification done on script file

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -17,19 +17,19 @@ set -x
 
 # List EC2 Instances
 echo "print the EC2 instances"
-aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' > file1.txt
 
 # List S3 Bucket
 echo "Print the list of S3 bucket"
-aws s3 ls
+aws s3 ls >> file1.txt
 
 # List IAM Users
 echo "print the list of IAM Users"
-aws iam list-users | jq '.Users[].UserName'
+aws iam list-users | jq '.Users[].UserName' >> file1.txt
 
 
 # List the Lambda functions
 echo "print all the lambda functions"
-aws lambda list-functions
+aws lambda list-functions >> file1.txt
 
 
